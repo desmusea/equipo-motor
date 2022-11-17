4c63079dd9d105e4f470bdd9c64987f71225cc6e
claraharguindey <clarisaharguindey@gmail.com>
2022-11-17 19:17:42 +0100

    add port env

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const cors = require('cors');
+const PORT = process.env.PORT || 3000;
 
 const corsOptions = {
   origin: '*',
@@ -50,6 +51,6 @@ app.get('/episodes', (req, res) => {
   res.send(episodes);
 });
 
-app.listen(3000, () => {
-  console.log('Server runs on port 3000 ✨');
+app.listen(PORT, () => {
+  console.log('Server is running ✨');
 });
